cec661856e09a15b197179e8742f17d061197988
Diego-Cuevas <60278677+Diego-Cuevas@users.noreply.github.com>
2022-06-17 22:45:43 -0500

    Create Circle_circumference.cpp
    Program to calculate the circumference of a circle.

---
+++ Circle_circumference.cpp
@@ -0,0 +1,17 @@
+//Diego Cuevas
+//Program to calculate the circumference of a circle
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	
+	int ratio = 15;
+	float pi = 3.1416, circumference = 0;
+	
+	circumference = 2 * pi * ratio;
+	
+	cout << "The size of circumference is " << circumference << endl;
+	
+	return 0;
+}
+
