a5b669b48927caeb50ea98a85fe96ee01a967054
Diego-Cuevas <60278677+Diego-Cuevas@users.noreply.github.com>
2022-06-18 21:27:10 -0500

    Create Square_Area.cpp
    A program that calculate the area of a square whose sides measure 5m

---
+++ Square_Area.cpp
@@ -0,0 +1,16 @@
+//Diego Cuevas
+//This program calculate the area of a square whose sides measure 5m
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    
+    int side = 5, area = 0;
+
+    area = side * side;
+
+    cout << "The area of the square is: " << area << "m" << endl;
+
+    return 0;
+}
+
