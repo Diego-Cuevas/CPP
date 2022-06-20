676f0b3a2d96d45a9c12f2bb611a40804d1b9b70
Diego-Cuevas <60278677+Diego-Cuevas@users.noreply.github.com>
2022-06-20 18:15:20 -0500

    Create Extraction_operator.cpp
    This program show how extraction operator work.

---
+++ Extraction_operator.cpp
@@ -0,0 +1,22 @@
+//Diego Cuevas
+//Extraction operator
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+
+    int age = 0, level = 0;
+    string name = " ";
+
+    cout << "Name: ";
+    cin >> name;
+    
+    cout << "Age: ";
+    cin >> age;
+
+    cout << "Level: ";
+    cin >> level;
+    
+    cout << "Name: " << name << endl << "Age: " << age << endl << "Level: " << level << endl;
+    return 0;
+}
