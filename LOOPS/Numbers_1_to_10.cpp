4cff898512931d562fd5e6c9f2d3eae6b5815cff
Diego-Cuevas <60278677+Diego-Cuevas@users.noreply.github.com>
2022-06-28 21:43:55 -0500

    Create Numbers_1_to_10.cpp
    This program print the numbers from 1 to 10.

---
+++ LOOPS/Numbers_1_to_10.cpp
@@ -0,0 +1,14 @@
+//Diego Cuevas
+//This program print the numbers from 1 to 10.
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    
+    for (int i = 0; i < 10; i++)
+    {
+        cout << i+1 << endl;
+    }
+    
+    return 0;
+}
