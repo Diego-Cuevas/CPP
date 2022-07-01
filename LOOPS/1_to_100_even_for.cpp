e75f224af56c5580263b61245fa82154a15e185b
Diego-Cuevas <60278677+Diego-Cuevas@users.noreply.github.com>
2022-06-30 21:58:12 -0500

    Create 1_to_100_even_for.cpp
    This program shows a list of even
    numbers from 1 to 100.

---
+++ LOOPS/1_to_100_even_for.cpp
@@ -0,0 +1,18 @@
+//Diego Cuevas
+//This program shows a list of even numbers from 1 to 100.
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+
+    for (int i = 1; i <= 100; i++)
+    {
+        if (i % 2 == 0)
+        {
+            cout << i << endl;
+        }
+        
+    }
+    
+    return 0;
+}
