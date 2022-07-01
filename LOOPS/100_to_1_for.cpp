ad082e40603ebc2fc486b4695a23938d8b360d2c
Diego-Cuevas <60278677+Diego-Cuevas@users.noreply.github.com>
2022-06-30 21:45:21 -0500

    Create 100_to_1_for.cpp
    This program show a list of numbers from 100 to 1.

---
+++ LOOPS/100_to_1_for.cpp
@@ -0,0 +1,14 @@
+//Diego Cuevas
+//This program show a list of numbers from 100 to 1.
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+
+    for (int i = 100; i >= 1; i--)
+    {
+        cout << i << endl;
+    }
+    
+    return 0;
+}
