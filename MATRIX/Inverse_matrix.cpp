2a481a74fee701da53d8163e9ec6c44a1a48348f
Diego-Cuevas <60278677+Diego-Cuevas@users.noreply.github.com>
2022-07-08 21:11:45 -0500

    Create Inverse_matrix.cpp
    This program request to user a matrix 3 by 3 and shows the inverse matrix.

---
+++ MATRIX/Inverse_matrix.cpp
@@ -0,0 +1,74 @@
+//Diego Cuevas
+//This program request to user a matrix 3 by 3 and shows the inverse matrix.
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+
+    float matrix[3][3] = {};
+
+    for (int i = 0; i < 3; i++) //Fill matrix
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cout << "Enter value for [" << i << "][" << j << "]: ";
+            cin >> matrix[i][j];
+        }
+        
+    }
+
+    cout << "Original matrix" << endl;
+
+    for (int i = 0; i < 3; i++) //Print matrix
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+
+    }
+
+    int determinant = matrix[0][0] * (matrix[1][1] * matrix[2][2] - matrix[2][1] * matrix[1][2]) +
+                      matrix[1][0] * (matrix[2][1] * matrix[0][2] - matrix[0][1] * matrix[2][2]) +
+                      matrix[2][0] * (matrix[0][1] * matrix[1][2] - matrix[1][1] * matrix[0][2]) ;
+    
+    float attached[3][3] = {}, inverse[3][3]; //Create array for attached and inverse matrix
+
+    for (int i = 0; i < 3; i++) //Calculate the attached matrix
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            attached[i][j] = (matrix[(i+1) % 3][(j+1) % 3] * matrix[(i+2) % 3][(j+2) % 3]
+                            - matrix[(i+1) % 3][(j+2) % 3] * matrix[(i+2) % 3][(j+1) % 3]);
+            inverse[i][j] = attached[i][j] / determinant;
+        }
+
+    }
+
+    cout << "Attached matrix" << endl;
+
+    for (int i = 0; i < 3; i++) //Print the attached matrix
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cout << attached[i][j] << " ";
+        }
+        cout << endl;
+
+    }
+
+    cout << "Inverse matrix" << endl;
+
+    for (int i = 0; i < 3; i++) //Print the attached matrix
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cout << inverse[i][j] << " ";
+        }
+        cout << endl;
+
+    }
+    
+    return 0;
+}
