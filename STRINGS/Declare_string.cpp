39d017bcfa3140ae713837cad89bf6158671b79d
Diego-Cuevas <60278677+Diego-Cuevas@users.noreply.github.com>
2022-07-11 18:10:22 -0500

    Create Declare_string.cpp
    This program declares a string.

---
+++ STRINGS/Declare_string.cpp
@@ -0,0 +1,19 @@
+//Diego Cuevas
+//This program declares a string.
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+
+    char name [6] = {""};
+
+    name[0] = 'D';
+    name[1] = 'i';
+    name[2] = 'e';
+    name[3] = 'g';
+    name[4] = 'o';
+
+    cout << name << endl; 
+    
+    return 0;
+}
