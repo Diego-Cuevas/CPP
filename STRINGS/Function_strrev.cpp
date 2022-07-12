d2861b4ea0878ecdbd3ed74966b71feb5912df29
Diego-Cuevas <60278677+Diego-Cuevas@users.noreply.github.com>
2022-07-11 19:41:38 -0500

    Create Function_strrev.cpp
    Function strnrev.

---
+++ STRINGS/Function_strrev.cpp
@@ -0,0 +1,14 @@
+//Diego Cuevas
+//Function strnrev.
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+
+    char name[6] = "Diego";
+
+    cout << "Name: " << strrev(name) << endl;
+    
+    return 0;
+}
